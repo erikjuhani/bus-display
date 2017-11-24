f780dd88f991c539487667ed51ca6a53c338d24c
Niko Tikkanen <niko@plexcellmedia.com>
2017-11-24 23:36:51 +0200

    front

---
+++ src/assets/js/slider.js
@@ -0,0 +1,32 @@
+
+var slides   = [];
+var curSlide = 0;
+
+$(function(){
+  loadSlides();
+  startSlider();
+  changeSlide();
+});
+
+function loadSlides(){
+  $("[data-type=slide]").each(function(){
+    var id = $(this).data('slide-id');
+    slides.push(id);
+  });
+}
+
+function startSlider(){
+  setInterval(function(){
+    changeSlide();
+  }, 5000);
+}
+
+function hideSlides(){
+  $("[data-type=slide]").hide();
+}
+
+function changeSlide(){
+  hideSlides();
+  $("[data-slide-id=" + slides[curSlide] + "]").fadeIn(300);
+  curSlide = curSlide++ >= slides.length - 1 ? 0 : curSlide++;
+}
