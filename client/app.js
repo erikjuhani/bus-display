5c4c0011c59167f1f26e8047eead94a8c261a1bd
erikjuhani <erik.kinn@gmail.com>
2017-11-25 20:03:37 +0200

    Changes

--- client/app.js
+++ client/app.js
@@ -1,12 +1,18 @@
 const server = {
   url: 'http://localhost',
   port: 3000
-}
-const map = L.map('map').setView([60, 24], 13);
+};
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+const options = {
+  zoomControl: false,
+};
+
+const map = L.map('map', options).setView([60, 24], 13);
+
+L.tileLayer('http://api.digitransit.fi/map/v1/{id}/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ',
+  id: 'hsl-map'}).addTo(map);
 
 let bus = L.marker([0, 0]).addTo(map);
 
